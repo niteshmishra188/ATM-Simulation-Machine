c61a803e649b8640dd08e90f3ec51ae419621073
Nitesh Kumar Mishra <127732043+niteshmishra188@users.noreply.github.com>
2024-09-24 16:22:09 +0530

    initial commit

---
+++ ATM-system/src/Conn.java
@@ -0,0 +1,19 @@
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","556624");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
